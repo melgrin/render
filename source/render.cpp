8e1732c37e203aa95833f4928e97898b5f648929
melgrin <flourhorn@gmail.com>
2021-09-13 01:06:29 -0400

    Line segments, small crosshair, rectangle.

--- source/render.cpp
+++ source/render.cpp
@@ -94,31 +94,44 @@ void fill_color(Offscreen_Bitmap_Buffer* bitmapBuffer, u32 color) {
     }
 }
 
-void draw_horizontal_line(Offscreen_Bitmap_Buffer* bitmapBuffer, int yOffset, u32 color) {
+void draw_horizontal_line_segment(Offscreen_Bitmap_Buffer* bitmapBuffer, int yOffset, int xStart, int xEnd, u32 color) {
 
     // The available memory is only as large as the window.  The mouse position should never be captured outside of the window, but the line position could be left over from previous, and the window was shrunk.  In that case, can't draw the line because it's outside the window and therefore out of bounds of the allocated bitmap buffer.
-    if (yOffset >= bitmapBuffer->height) { // yOffset starts at 0, so == height is invalid
+    if (yOffset >= bitmapBuffer->height) {
         return;
     }
+
+    assert(xStart < xEnd);
+
+    if (xStart < 0) xStart = 0;
+    if (xEnd >= bitmapBuffer->width) xEnd = bitmapBuffer->width;
 
     u8* row = (u8*) bitmapBuffer->memory;
     row += yOffset * bitmapBuffer->pitch;
     u32* pixel = (u32*) row;
-    for (int x = 0; x < bitmapBuffer->width; ++x) {
+    pixel += xStart;
+    for (int x = xStart; x < xEnd; ++x) {
         *pixel = color;
         ++pixel;
     }
 }
 
-void draw_vertical_line(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, u32 color) {
-
-    // See note in game_draw_horizontal_line.  Would exceed bitmap buffer size.
+void draw_vertical_line_segment(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, int yStart, int yEnd, u32 color) {
+    
+    // See note in game_draw_horizontal_line_segment.  Would exceed bitmap buffer size.
     if (xOffset >= bitmapBuffer->width) { // xOffset starts at 0, so == width is invalid
         return;
     }
 
+    assert(yStart < yEnd);
+
+    if (yStart < 0) yStart = 0;
+    if (yEnd >= bitmapBuffer->height) yEnd = bitmapBuffer->height;
+
     u8* row = (u8*) bitmapBuffer->memory;
-    for (int y = 0; y < bitmapBuffer->height; ++y) {
+    row += bitmapBuffer->pitch * yStart;
+
+    for (int y = yStart; y < yEnd; ++y) {
         u32* pixel = (u32*) row;
         pixel += xOffset;
         *pixel = color;
@@ -130,13 +143,15 @@ void draw_crosshair(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, int yOff
 
     local_persist u32 COLOR_HORIZONTAL = get_color(255, 0, 0);
     local_persist u32 COLOR_VERTICAL = get_color(0, 255, 0);
-    local_persist u32 COLOR_BACKGROUND = get_color(100, 100, 100);
+    local_persist u32 COLOR_BACKGROUND = get_color(0, 0, 0);
 
     //Platform::DEBUG_printf("drawing at {%4i, %4i}\n");
 
     fill_color(bitmapBuffer, COLOR_BACKGROUND);
-    draw_horizontal_line(bitmapBuffer, yOffset, COLOR_HORIZONTAL);
-    draw_vertical_line(bitmapBuffer, xOffset, COLOR_VERTICAL);
+    //draw_horizontal_line_segment(bitmapBuffer, yOffset, 0, bitmapBuffer->width-1, COLOR_HORIZONTAL);
+    //draw_vertical_line_segment(bitmapBuffer, xOffset, 0, bitmapBuffer->height-1, COLOR_VERTICAL);
+    draw_horizontal_line_segment(bitmapBuffer, yOffset, xOffset - 100, xOffset + 100, COLOR_HORIZONTAL);
+    draw_vertical_line_segment(bitmapBuffer, xOffset, yOffset - 100, yOffset + 100, COLOR_VERTICAL);
 }
 
 void draw_grid(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, int yOffset) {
@@ -148,18 +163,40 @@ void draw_grid(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, int yOffset) 
     fill_color(bitmapBuffer, COLOR_BG);
 
     for (int y = yOffset; y < bitmapBuffer->height; y += GRID_SPACING) {
-        draw_horizontal_line(bitmapBuffer, y, COLOR_FG);
+        draw_horizontal_line_segment(bitmapBuffer, y, 0, bitmapBuffer->width-1, COLOR_FG);
     }
     for (int y = yOffset; y >= 0; y -= GRID_SPACING) {
-        draw_horizontal_line(bitmapBuffer, y, COLOR_FG);
+        draw_horizontal_line_segment(bitmapBuffer, y, 0, bitmapBuffer->width-1, COLOR_FG);
     }
 
     for (int x = xOffset; x < bitmapBuffer->width; x += GRID_SPACING) {
-        draw_vertical_line(bitmapBuffer, x, COLOR_FG);
+        draw_vertical_line_segment(bitmapBuffer, x, 0, bitmapBuffer->height-1, COLOR_FG);
     }
     for (int x = xOffset; x > 0; x -= GRID_SPACING) {
-        draw_vertical_line(bitmapBuffer, x, COLOR_FG);
+        draw_vertical_line_segment(bitmapBuffer, x, 0, bitmapBuffer->height-1, COLOR_FG);
     }
+}
+
+struct Rectangle {
+    int x;
+    int y;
+    int w;
+    int h;
+    u32 border_color;
+    //u32 fill_color;
+};
+
+void draw_rect(Offscreen_Bitmap_Buffer* bitmapBuffer, int xOffset, int yOffset, Rectangle* rect) {
+    local_persist u32 COLOR_BG = get_color(60, 60, 60);
+    fill_color(bitmapBuffer, COLOR_BG);
+
+    local_persist u16 RECT_SIZE = 40;
+    local_persist u32 COLOR_FG = get_color(120, 200, 255);
+    draw_horizontal_line_segment(bitmapBuffer, yOffset            , xOffset, xOffset + RECT_SIZE, COLOR_FG); // top
+    draw_horizontal_line_segment(bitmapBuffer, yOffset + RECT_SIZE, xOffset, xOffset + RECT_SIZE, COLOR_FG); // bottom
+    draw_vertical_line_segment  (bitmapBuffer, xOffset            , yOffset, yOffset + RECT_SIZE, COLOR_FG); // left
+    draw_vertical_line_segment  (bitmapBuffer, xOffset + RECT_SIZE, yOffset, yOffset + RECT_SIZE, COLOR_FG); // right
+
 }
 
 struct Button_State {
@@ -205,7 +242,14 @@ void update(Memory* memory, Keyboard_Input* keyboard, Mouse_Input* mouse, Offscr
     assert(sizeof(State) <= memory->permanentStorageSize);
     State* state = (State*) memory->permanentStorage;
     if (!memory->isInitialized) {
-        // ...
+
+        Rectangle* rect = (Rectangle*) memory->permanentStorage;
+        rect->x = 100;
+        rect->y = 500;
+        rect->w = 400;
+        rect->h = 120;
+        rect->border_color = get_color(255, 0, 0);
+
         memory->isInitialized = true;
     }
 
@@ -227,7 +271,11 @@ void update(Memory* memory, Keyboard_Input* keyboard, Mouse_Input* mouse, Offscr
     //draw_weird_gradient(bitmapBuffer, state->xOffset, state->yOffset);
     //draw_test_color_bands(bitmapBuffer);
     //draw_crosshair(bitmapBuffer, state->xOffset, state->yOffset);
-    draw_grid(bitmapBuffer, state->xOffset, state->yOffset);
+    //draw_grid(bitmapBuffer, state->xOffset, state->yOffset);
+
+    draw_rect(bitmapBuffer, state->xOffset + 20, state->yOffset + 40, (Rectangle*) memory->permanentStorage);
+
+    // TODO draw arbitrary points, then preserve their position while panning
 
 }
 
